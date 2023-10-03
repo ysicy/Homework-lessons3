3543bf063444b61aa57640f2273bf57d61fb83d8
ysicy <123982423+ysicy@users.noreply.github.com>
2023-10-03 15:35:59 +0300

    update!!!

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -6,8 +6,8 @@ public class CalendarComponent {
 
 public void setDate (String day, String month, String year){
     $("#dateOfBirthInput").click();
-    $(".react-datepicker__month-select").selectOption("September");
-    $(".react-datepicker__year-select").selectOption("2023");
-    $(".react-datepicker__day--002").click();
+    $(".react-datepicker__month-select").selectOption(month);
+    $(".react-datepicker__year-select").selectOption(year);
+    $(".react-datepicker__day--0" + day).click();
 }
 }
