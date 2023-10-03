ff6fce8298dd1f2f5281e7da2a548cf59d753bd8
ysicy <123982423+ysicy@users.noreply.github.com>
2023-10-03 16:16:44 +0300

    done 16.05...

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -11,9 +11,6 @@ public void setDate (String day, String month, String year){
     $(".react-datepicker__year-select").selectOption(year);
     $(".react-datepicker__day--0" + day).click();
 
-    $(".react-datepicker__month-select").selectOption(month);
-    $(".react-datepicker__year-select").selectOption(year);
-    $(".react-datepicker__day--0" + day).click();
 
 }
 }
